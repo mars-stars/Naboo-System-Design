dc302147f0281388949f8611fe355af7d6d5c79f
Leslie Rodriguez <rleslie1015@gmail.com>
2021-05-10 21:33:00 -0500

    removes unused code

--- controllers/styles.js
+++ controllers/styles.js
@@ -7,16 +7,7 @@ module.exports = {
         console.log(err)
         res.sendStatus(400)
       } else {
-
-
-        // // test appending properties here
-        // product = json.stringify(product);
         res.status(200).json(product);
-        // res.status(200).send(product);
-        // try json.stringify product
-        // .json(json.parse(product))
-
-        // try send instead .json
       }
     })
   },
