de174259dc63cc9f95a8e016149edef37fd7af27
Leslie Rodriguez <rleslie1015@gmail.com>
2021-05-15 12:59:35 -0500

    added cache for products endppint

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,10 @@
 const models = require('../models');
+const nodeCache = require('node-cache');
+const cache = new nodeCache({ maxKeys: 100 });
 
 module.exports = {
   get: function(req, res) {
+
     models.products.getOneProduct(req.params.id, (err, product) => {
       if (err) {
         res.sendStatus(400)
@@ -11,12 +14,22 @@ module.exports = {
     })
   },
   getAll: function(req, res) {
-    models.products.getAll(req.query, (err, products) => {
-      if (err) {
-        res.sendStatus(400)
-      } else {
-        res.status(200).json(products);
-      }
-    })
+    let page = req.query.page || 1;
+    let limit = req.query.limit || 5;
+
+    let cacheKey = `/products?p=${page},l=${limit}`;
+
+    if (cache.has(cacheKey)) {
+      res.status(200).send(cache.get(cacheKey));
+    }  else {
+      models.products.getAll(req.query, (err, products) => {
+        if (err) {
+          res.sendStatus(400)
+        } else {
+          cache.set(cacheKey, products);
+          res.status(200).json(products);
+        }
+      })
+    }
   }
  };
