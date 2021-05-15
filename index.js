f7d62778dc75677e0ef8b9dcb840f886111f5372
Leslie Rodriguez <rleslie1015@gmail.com>
2021-05-15 12:42:06 -0500

    added logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var db = require('./db/index.js');
 const server = require('./server/index');
+var logger = require('morgan')
 
 const app = express();
 
@@ -8,9 +9,11 @@ const port = 3000;
 
 app.use(express.json());
 
+app.use(logger('dev'));
+
 app.use('/', server);
 
-app.get('/', (req, res) => res.json({ message: 'Hello World' }))
+app.get('/', (req, res) => res.status(200).json({ message: 'Hello World' }))
 
 app.use('/', express.static(`${__dirname}/loader`));
 
