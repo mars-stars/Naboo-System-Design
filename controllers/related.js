1532ed26ab8f2b554028a562b01375c03d2eb3c0
Leslie Rodriguez <rleslie1015@gmail.com>
2021-05-15 13:35:52 -0500

    adds cache for related endpoint

--- controllers/related.js
+++ controllers/related.js
@@ -1,14 +1,24 @@
 const models = require('../models');
+const nodeCache = require('node-cache');
+const cache = new nodeCache({ maxKeys: 50000 });
 
 module.exports = {
   getRelatedProducts: function(req, res) {
-    models.related.getRelatedByProductId(req.params.id, (err, products) =>{
-      if (err) {
-        res.sendStatus(400)
-      } else {
-        res.status(200).json(products);
-      }
-    })
+    let id = req.params.id;
+    let cacheKey = `/products/${id}/related`;
+
+    if (cache.has(cacheKey)) {
+      res.status(200).send(cache.get(cacheKey));
+    } else {
+      models.related.getRelatedByProductId(req.params.id, (err, products) =>{
+        if (err) {
+          res.sendStatus(400)
+        } else {
+          cache.set(cacheKey, products);
+          res.status(200).send(products);
+        }
+      })
+    }
   },
   getAll: function(req, res) {
     models.related.getAll((err, products) => {
